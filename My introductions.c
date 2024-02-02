5b753084868179558c689e3a4ed28d5c41befa78
Philipwaweru <157624709+Philipwaweru@users.noreply.github.com>
2024-02-02 21:13:04 +0300

    Add files via upload

---
+++ My introductions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Philip Munene\n");
+     printf("My course is Bcs in Civil Engineering\n");
+      printf("My favorite team is Manchester City \n");
+       printf("My favorite meal is anything with potatoes\n");
+    return 0;
+}

add872e6a98ac87e7c5668ea1f65aed36c85039e
Philipwaweru <157624709+Philipwaweru@users.noreply.github.com>
2024-03-11 18:37:55 +0300

    Chapter 9 example

--- main.c
+++ main.c
@@ -1,39 +1,48 @@
 #include <stdio.h>
 
-struct Record
-{
-  char firstname[20], lastname[20];
-  int yearborn;
-};
-struct Record records[5] = {
-  {"Philip", "Munene", 2004},
-  {"Jane", "Wangare", 1999},
-  {"Bob", "Mutuku", 2001},
-  {"Alice", "Atieno", 2005},
-  {"Charlie", "Nyambane", 1997}
-};
-void sortRecords(struct Record *records, int n)
-{
-  for (int i = 0; i < n - 1; i++)
-  {
-    for (int j = 0; j < n - i - 1; j++)
-    {
-      if (records[j].yearborn > records[j + 1].yearborn)
-      {
-        struct Record temp = records[j];
-        records[j] = records[j + 1];
-        records[j + 1] = temp;
-      }
-    }
-  }
+#define MAX 10
+
+int a[MAX];
+int rand_seed = 10;
+
+/*
+ * From K&R - produces an integer random number
+ * between 0 and 32767.
+ */
+int rand(void) {
+    rand_seed = rand_seed * 1103515245 + 12345;
+    return (unsigned int)(rand_seed / 65536) % 32768;
 }
-int main()
-{
-  int n = 5;
-  sortRecords(records, n);
-  for (int i = 0; i < n; i++)
-  {
-    printf("%s %s %d\n", records[i].firstname, records[i].lastname, records[i].yearborn);
-  }
-  return 0;
+
+void bubble_sort(int m) {
+    int x, y, t;
+    for (x = 0; x < m - 1; x++) {
+        for (y = 0; y < m - x - 1; y++) {
+            if (a[y] > a[y + 1]) {
+                t = a[y];
+                a[y] = a[y + 1];
+                a[y + 1] = t;
+            }
+        }
+    }
+}
+
+int main(void) {
+    int i;
+
+    /* Fill array */
+    for (i = 0; i < MAX; i++) {
+        a[i] = rand();
+        printf("%d\n", a[i]);
+    }
+
+    bubble_sort(MAX);
+
+    /* Print sorted array */
+    printf("--------------\n");
+    for (i = 0; i < MAX; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
 }
